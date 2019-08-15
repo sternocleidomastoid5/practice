0cdf4e1ed2f889977b7bd81facbeb1944bbeb3ab
Pav <p.mohyluk@gmail.com>
2019-08-15 15:53:15 +0100

    First commit

---
+++ Practice.js
@@ -0,0 +1,29 @@
+/*let space1 = "x";
+let space2 = "o";
+let space3 = "";
+let space4 = "x";
+let space5 = "x";
+let space6 = "";
+let space7 = "o";
+let space8 = "";
+let space9 = "";
+
+console.log("    |   |   ");
+console.log(`  ${space1} | ${space2} |  ${space3} `);
+console.log("    |   |   ");
+console.log("-------------");
+console.log("    |   |   ");
+console.log(` ${space4}  | ${space5} |  ${space6} `);
+console.log("    |   |   ");
+console.log("-------------");
+console.log("    |   |   ");
+console.log(`  ${space7} |  ${space8} |  ${space9} `);
+console.log("    |   |   ");
+*/
+
+/*let myName = "Pav";
+let myAge = 27;
+let favColour = "Yellow";
+console.log(`My name is ${myName}, I am ${myAge} years old, and my favourite colour is ${favColour}.`);
+*/
+console.log("Wa-Wa-WEE-WA!");
